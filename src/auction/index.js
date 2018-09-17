e90a986f5e58a71383ff5e63ef4be326bc726a60
mylo <imylomylo@gmail.com>
2018-09-18 03:27:17 +1000

    rejects completed for now

--- src/auction/index.js
+++ src/auction/index.js
@@ -2,12 +2,13 @@
 // auctionaddress [pubkey]
 export function auctionaddress(rpc) {
     return new Promise((resolve, reject) => {
-        rpc.auctionaddress().then(resp => {
-            resolve(resp)
-        }).catch(error => {
-            console.log(error)
-            reject(error)
-        })
+        // rpc.auctionaddress().then(resp => {
+        //     resolve(resp)
+        // }).catch(error => {
+        //     console.log(error)
+        //     reject(error)
+        // })
+        reject("auctionaddress - Not supported yet")
     })
 }
 
